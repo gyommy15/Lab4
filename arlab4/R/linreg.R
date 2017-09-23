08c430a2d6e9d686fa2af01a8695c2a224d177d1
Josh <gyommy15@gmail.com>
2017-09-23 17:56:16 +0200

    progress

--- arlab4/R/linreg.R
+++ arlab4/R/linreg.R
@@ -1,8 +1,45 @@
-linreg <- function(formula, data){
-
-  x <- model.matrix()
-  y <- all.vars()
-
-  return()
-
-}
+linreg <- setRefClass("linreg", 
+  fields = list(formula="formula",
+                data="data.frame",
+                reg_coe="numeric",
+                fit_val="numeric",
+                residu="numeric",
+                dof="numeric",
+                res_var="numeric",
+                var_reg_coe="numeric",
+                t_val="numeric"),
+  
+  methods = list(
+    initialize = function(formula, data){
+      
+      formula <<- formula
+      data <<- data
+      
+      X <- model.matrix(formula, data)
+      y <- all.vars(formula)
+      
+      model1 <- lm(y ~ X, data = data)
+      
+      reg_coe <<- model1$coefficients
+      fit_val <<- model1$fitted.values
+      residu <<- model1$residuals
+      dof <<- model1$df.residual
+      res_var <<- (t(residu)%*%residu)/dof
+      var_reg_coe <<- var(reg_coe)
+      t_val <<- reg_coe/sd(reg_coe)
+      
+    },
+    print = function(){
+      "Print out the coefficients and coefficient names"
+      cat("Call: /n")
+      cat(paste0("linreg(formula = ", format(formula),", data = ", format(data),")\n\n"))
+      cat("Coefficients: /n")
+      reg_coe
+      
+    }
+    
+    
+        
+    
+  )
+  )
--- arlab4/tests/testthat/test_linreg_ref_class.R
+++ arlab4/tests/testthat/test_linreg_ref_class.R
@@ -19,8 +19,8 @@ test_that("class is correct", {
 test_that("print() method works", {
   linreg_mod <- linreg$new(Petal.Length~Sepal.Width+Sepal.Length, data=iris)
   
-  expect_output(linreg_mod$print(),"linreg(formula = Petal.Length ~ Sepal.Width + Sepal.Length, data = iris)")  
-  expect_output(linreg_mod$print()," (Intercept)   Sepal.Width  Sepal.Length ")  
+  expect_output(linreg_mod$print(),"linreg\\(formula = Petal\\.Length ~ Sepal\\.Width \\+ Sepal\\.Length, data = iris\\)")
+  expect_output(linreg_mod$print()," \\(Intercept\\)   Sepal\\.Width  Sepal\\.Length")
 })
 
 test_that("pred() method works", {
@@ -50,3 +50,4 @@ test_that("summary() method works", {
   expect_output(linreg_mod$summary(), "Sepal.Length( )*1.7[0-9]*( )*0.0[0-9]*( )*27.5[0-9]*( )*.*( )*\\*\\*\\*")
   expect_output(linreg_mod$summary(), "Residual standard error: 0.6[0-9]* on 147 degrees of freedom")
 })
+
